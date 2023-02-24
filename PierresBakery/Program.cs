7563e2c3f428e3f6e146569fe7257f1df167cb0a
E Luckie <eluckie.d@gmail.com>
2023-02-24 12:11:00 -0700

    slightly refactor to clarify and DRY up code

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -15,11 +15,11 @@ namespace PierresBakery
       Console.WriteLine("BREAD - $5 per loaf or Buy 2, Get 1 free");
       Console.WriteLine("------------------------------------------------------------------");
       Console.WriteLine("Please enter how many pastries you'd like:");
-      string stringNumber1 = Console.ReadLine();  
+      string pastryQtyString = Console.ReadLine();  
       Console.WriteLine("Please enter how many loaves of bread you'd like:");
-      string stringNumber2 = Console.ReadLine();  
-      int pastryCount = int.Parse(stringNumber1);  
-      int breadCount = int.Parse(stringNumber2);  
+      string breadQtyString = Console.ReadLine();  
+      int pastryCount = int.Parse(pastryQtyString);  
+      int breadCount = int.Parse(breadQtyString);  
       Pastry pastryOrder = new Pastry(pastryCount);
       Bread breadOrder = new Bread(breadCount);
       ConfirmOrEditOrder(pastryOrder, breadOrder);
@@ -29,7 +29,7 @@ namespace PierresBakery
       Console.WriteLine("Please confirm that you entered your order correctly:");
       Console.WriteLine($"You've chosen {pastryOrder.PastryCount} pastries");
       Console.WriteLine($"and {breadOrder.BreadCount} loaves of bread");
-      Console.WriteLine("Is this correct? Enter 'yes' to confirm, or 'no' to re-enter your desired quantities");
+      Console.WriteLine("Is this correct? Enter 'yes' to confirm, or enter any key to update your desired quantities");
       string userInput = Console.ReadLine();  
       if (userInput.ToUpper() == "YES")
       {
@@ -39,11 +39,11 @@ namespace PierresBakery
       {
         Console.WriteLine("Let's update your order! Please enter your desired quantities again.");
         Console.WriteLine("Please enter how many pastries you'd like:");
-        string stringNumber1 = Console.ReadLine();  
+        string pastryQtyString = Console.ReadLine();  
         Console.WriteLine("Please enter how many loaves of bread you'd like:");
-        string stringNumber2 = Console.ReadLine();  
-        int pastryCount = int.Parse(stringNumber1);  
-        int breadCount = int.Parse(stringNumber2);  
+        string breadQtyString = Console.ReadLine();  
+        int pastryCount = int.Parse(pastryQtyString);  
+        int breadCount = int.Parse(breadQtyString);  
         Pastry updatedPastryOrder = new Pastry(pastryCount);
         Bread updatedBreadOrder = new Bread(breadCount);
         ConfirmOrEditOrder(updatedPastryOrder, updatedBreadOrder);
@@ -52,16 +52,14 @@ namespace PierresBakery
     }
     static void CalculateGrandTotal(Pastry pastryOrder, Bread breadOrder)
     {
-      int breadQuantity = breadOrder.BreadCount;
-      int pastryQuantity = pastryOrder.PastryCount;
       int breadTotal = breadOrder.CalculateTotal();
       int pastryTotal = pastryOrder.CalculateTotal();
       int grandTotal = breadTotal + pastryTotal;
       Console.WriteLine("Calculating your total...");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~**~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("CONFIRMED");
-      Console.WriteLine(pastryQuantity + " pastries cost $" + pastryTotal);
-      Console.WriteLine(breadQuantity + " loaves of bread cost $" + breadTotal);
+      Console.WriteLine(pastryOrder.PastryCount + " pastries cost $" + pastryTotal);
+      Console.WriteLine(breadOrder.BreadCount+ " loaves of bread cost $" + breadTotal);
       Console.WriteLine("Your grand total is $" + grandTotal);
       Console.WriteLine("Would you like to place a new order?");
       Console.WriteLine("Please enter 'yes' to place a new order. To exit, enter any key.");
@@ -72,7 +70,8 @@ namespace PierresBakery
       }
       else
       {
-        Console.WriteLine("Thank you for your order!");
+        Console.WriteLine("Thank you for your order");
+        Console.WriteLine("Have the best day!");
       }
     }
   }
