bfe6026c57d924148a0925c3436ee7c0ed0ea931
Luckie <eluckie.d@gmail.com>
2023-02-24 09:06:05 -0700

    Initial commit

---
+++ ProjectName/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using ProjectName.Models;
+using System.Collections.Generic;
+
+class Program
+{
+  static void Main()
+  {
+    // C# Program code goes here.
+  }
+}
---
+++ UserInterfaceModels/Banners.cs
@@ -0,0 +1,19 @@
+namespace ProgramName.UserInterfaceModels
+{
+  class WelcomeBanner
+  {
+    public static string Welcome { get; } = @"
+  
+    ";
+  }
+
+  class GoodbyeBanner
+  {
+    // GoodbyeBanner code here.
+  }
+
+  class SubMenuBanner
+  {
+    // SubMenuBanner code here.
+  }
+}
